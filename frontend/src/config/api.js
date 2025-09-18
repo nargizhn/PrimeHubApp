3a161ea793c417a8897f73c5123359c5e988afca
Huseyn <58394849+hhuseynli@users.noreply.github.com>
2025-09-18 22:39:35 +0400

    changed api url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 // API Configuration
 // Change this URL to point to your deployed backend or local development server
-const API_BASE_URL = process.env.REACT_APP_API_URL || "https://backend-776187932427.europe-west1.run.app";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://backend-905509257688.europe-west1.run.app";
 
 export const API_ENDPOINTS = {
   BASE_URL: API_BASE_URL,
